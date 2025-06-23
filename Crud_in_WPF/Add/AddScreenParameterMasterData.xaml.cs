e1c0771404ba70d2ac45c3ef878adefe0b470532
Saurabh Kushwaha <saurabh.kushwaha@sayalife.in>
2025-06-23 18:05:00 +0530

    Project Setup

---
+++ Crud_in_WPF/Add/AddScreenParameterMasterData.xaml.cs
@@ -0,0 +1,224 @@
+﻿using ArkaPRP83.Core.Helper;
+using log4net;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Crud_in_WPF.Add
+{
+    public partial class AddScreenParameterMasterData : Window
+    {
+        private static readonly ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private int screenId;
+        private string cs = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+
+        public AddScreenParameterMasterData()
+        {
+            try
+            {
+
+                InitializeComponent();
+                addScreenList();
+                screenName.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Error Message - " + ex.Message + "\n" + ex.StackTrace);
+            }
+        }
+
+        public void addScreenList()
+        {
+            try
+            {
+
+                DataTable dt = SqlHelper.ExecuteDataTable(cs, CommandType.StoredProcedure, "GetScreenNameNotDeleted");
+                screenName.ItemsSource = dt.DefaultView;
+                screenName.DisplayMemberPath = "Name";
+                screenName.SelectedValuePath = "Id";
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Error Message - " + ex.Message + "\n" + ex.StackTrace);
+            }
+
+            //using (SqlConnection con = new SqlConnection(cs))
+            //{
+            //    con.Open();
+            //    SqlCommand cmd = new SqlCommand("GetScreenNameNotDeleted", con);
+            //    cmd.CommandType = CommandType.StoredProcedure;
+            //    SqlDataAdapter da = new SqlDataAdapter(cmd);
+            //    DataTable dt = new DataTable();
+            //    da.Fill(dt);
+            //    da.Dispose();
+            //    screenName.ItemsSource = dt.DefaultView;
+            //    screenName.DisplayMemberPath = "Name";
+            //    screenName.SelectedValuePath = "Id";
+            //}
+        }
+
+        private void screenName_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            if (screenName.SelectedValue != null)
+            {
+                screenId = Convert.ToInt32(screenName.SelectedValue);
+            }
+        }
+        private void addNewScreenPrmt_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int id = screenId;
+                string name = txtSM_Name.Text.ToString();
+                string x = txtSequence.Text;
+                int sequenceNumber;
+                bool IsActive = isActive.IsChecked ?? false;
+                if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(x))
+                {
+                    MessageBox.Show("Please Fill both fields!!", "Error Message", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                if (!int.TryParse(x, out sequenceNumber) || sequenceNumber == 0 || sequenceNumber > 100)
+                {
+                    MessageBox.Show("Sequence number contain only an integer Value and number between 1-100 !!",
+                            "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                    return;
+                }
+                if (checkSequenceNumber(screenId, sequenceNumber))
+                {
+                    MessageBox.Show("Already a screen exist at this sequence Number , Please enter another Sequence Number!!", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                    return;
+                }
+                if (!string.IsNullOrEmpty(name) && id > 0)
+                {
+                    Add(id, name, IsActive, sequenceNumber);
+                }
+                else
+                {
+                    MessageBox.Show("Please fill all columns!!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Error Message - " + ex.Message + "\n" + ex.StackTrace);
+            }
+        }
+
+        private bool checkSequenceNumber(int screenId, int sequenceNumber)
+        {
+            try
+            {
+                int row = (int)SqlHelper.ExecuteScalar(cs, CommandType.StoredProcedure, "GetCountSequenceExistScreenParameterMaster",
+                   screenId, sequenceNumber);
+                if (row > 0)
+                {
+                    return true;
+                }
+                return false;
+
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Error Message - " + ex.Message + "\n" + ex.StackTrace);
+                throw ex;
+            }
+
+            //using (SqlConnection con = new SqlConnection(cs))
+            //{
+            //    con.Open();
+            //    string query = "Select Count(*) From [ScreenParameterMaster] Where SequenceNumber = @sequenceNumber";
+            //    SqlCommand cmd = new SqlCommand(query, con);
+            //    cmd.Parameters.AddWithValue("@sequenceNumber", sequenceNumber);
+            //    int row = (int)cmd.ExecuteScalar();
+            //    if (row > 0)
+            //    {
+            //        return true;
+            //    }
+            //    return false;
+            //}
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void Add(int masterId, string Name, bool isActive, int sequenceNumber)
+        {
+            try
+            {
+                bool isDeleted = false;
+                int row = SqlHelper.ExecuteNonQuery(cs, "InsertIntoScreenParameterMaster", masterId, Name, isActive, isDeleted, sequenceNumber);
+                if (row > 0)
+                {
+                    MessageBox.Show("Data inserted successfully!!", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                    txtSM_Name.Clear();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Data insertion failed!!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Error Message - " + ex.Message + "\n" + ex.StackTrace);
+            }
+            //using (SqlConnection con = new SqlConnection(cs))
+            //{
+            //    con.Open();
+            //    SqlCommand cmd = new SqlCommand("InsertIntoScreenParameterMaster", con);
+            //    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            //    cmd.Parameters.AddWithValue("@ScreenMasterId", masterId);
+            //    cmd.Parameters.AddWithValue("@Name", Name);
+            //    cmd.Parameters.AddWithValue("@sequenceNumber", sequenceNumber);
+            //    cmd.Parameters.AddWithValue("@isActive", isActive);
+            //    cmd.Parameters.AddWithValue("@isDeleted", false);
+            //    int row = cmd.ExecuteNonQuery();
+            //    if (row > 0)
+            //    {
+            //        MessageBox.Show("Data inserted successfully!!", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            //        txtSM_Name.Clear();
+            //        this.Close();
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("Data insertion failed!!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            //    }
+            //}
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtSM_Name.Clear();
+            txtSM_Name.Focus();
+        }
+
+        private void txtSM_Name_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtSM_Name.Text))
+                txtplaceholder.Visibility = Visibility.Visible;
+            else
+                txtplaceholder.Visibility = Visibility.Hidden;
+        }
+
+        private void txtSequence_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtSequence.Text))
+                txtplaceholder2.Visibility = Visibility.Visible;
+            else
+                txtplaceholder2.Visibility = Visibility.Hidden;
+        }
+
+        private void btnClear2_Click(object sender, RoutedEventArgs e)
+        {
+            txtSequence.Clear();
+            txtSequence.Focus();
+        }
+    }
+}
---
+++ Crud_in_WPF/ScreenAndParameter/ScreenParameterMasterPage.xaml.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Data;
+using Crud_in_WPF.Add;
+using Crud_in_WPF.Update;
+using ArkaPRP83.Core.Helper;
+using System.Configuration;
+using log4net;
+
+namespace Crud_in_WPF.ScreenAndParameter
+{
+    public partial class ScreenParameterMasterPage : Window
+    {
+        private static readonly ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private int ScreenParameterMaster_Id;
+        private int ScreenMaster_Id;
+        string cs = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+        public ScreenParameterMasterPage()
+        {
+            try
+            {
+                logger.Info("Enter Screen Parameter Master Page.");
+                InitializeComponent();
+                addDropdownListData();
+                screenParameterMasterData();
+                screenMasterName.SelectedIndex = 0;
+
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Error Message - " + ex.Message + "\n" + ex.StackTrace);
+            }
+        }
+
+        public void addDropdownListData()
+        {
+
+            try
+            {
+                DataTable dt = SqlHelper.ExecuteDataTable(cs, CommandType.StoredProcedure, "GetScreenNameNotDeleted");
+                screenMasterName.ItemsSource = dt.DefaultView;
+                screenMasterName.DisplayMemberPath = "Name";
+                screenMasterName.SelectedValuePath = "Id";
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Error Message - " + ex.Message + "\n" + ex.StackTrace);
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void screenMasterName_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                if (screenMasterName.SelectedValue != null)
+                {
+                    ScreenMaster_Id = Convert.ToInt32(screenMasterName.SelectedValue);
+                    screenParameterMasterData(ScreenMaster_Id);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Error Message - " + ex.Message + "\n" + ex.StackTrace);
+            }
+        }
+        public void screenParameterMasterData(int id = 1)
+        {
+            try
+            {
+                DataTable dt = SqlHelper.ExecuteDataTable(cs, "GetScreenParameterNameBasedOnScreenMaster", id);
+                dataGridScreenParameterMasterPage.ItemsSource = dt.DefaultView;
+                dataGridScreenParameterMasterPage.SelectedValuePath = "Id";
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Error Message - " + ex.Message + "\n" + ex.StackTrace);
+            }
+        }
+
+
+        private void dataGridScreenParameterMasterPage_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (dataGridScreenParameterMasterPage.SelectedValue != null)
+            {
+                ScreenParameterMaster_Id = Convert.ToInt32(dataGridScreenParameterMasterPage.SelectedValue);
+            }
+        }
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                logger.Info("Clicked Add Screen Parameter Master Button.");
+                AddScreenParameterMasterData addScreenParemeterMasterData = new AddScreenParameterMasterData();
+                addScreenParemeterMasterData.ShowDialog();
+                screenParameterMasterData(ScreenMaster_Id);
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Error Message - " + ex.Message + "\n" + ex.StackTrace);
+            }
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                logger.Info("Clicked Edit Screen Parameter Master Button.");
+                if (ScreenParameterMaster_Id > 0 && ScreenMaster_Id > 0)
+                {
+                    EditScreenParameterMasterData edit = new EditScreenParameterMasterData(ScreenParameterMaster_Id, ScreenMaster_Id);
+                    edit.ShowDialog();
+                    screenParameterMasterData(ScreenMaster_Id);
+                }
+                else
+                    MessageBox.Show("Plaese select an item for Edit!!!", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Error Message - " + ex.Message + "\n" + ex.StackTrace);
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                logger.Info("Clicked Delete Screen Parameter Master Button.");
+                if (ScreenParameterMaster_Id > 0)
+                {
+                    MessageBoxResult result = MessageBox.Show("Are You sure to delete this Parameter?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        screenParameterDelete(ScreenParameterMaster_Id);
+                        screenParameterMasterData(ScreenMaster_Id);
+                    }
+                    else
+                    {
+                        screenParameterMasterData(ScreenMaster_Id);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Select an item first to Delete!!", "Information", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Error Message - " + ex.Message + "\n" + ex.StackTrace);
+            }
+        }
+
+        private void screenParameterDelete(int ScreenParameterMaster_Id)
+        {
+            try
+            {
+
+                if (ScreenParameterMaster_Id > 0)
+                {
+                    int row = SqlHelper.ExecuteNonQuery(cs, "DeleteScreenParameterMaster", ScreenParameterMaster_Id);
+                    if (row > 0)
+                    {
+                        MessageBox.Show("Data Deleted Successfully!!", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data didn't Delete!!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Error Message - " + ex.Message + "\n" + ex.StackTrace);
+            }
+        }
+
+        private void Image_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
